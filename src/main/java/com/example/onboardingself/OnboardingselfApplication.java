4026a8b5f3fddea5975bdb26a48a3a3de7a2ff9b
horobito <odkfu1@gmail.com>
2022-07-17 18:52:37 +0900

    feat: init setting

---
+++ src/main/java/com/example/onboardingself/OnboardingselfApplication.java
@@ -0,0 +1,13 @@
+package com.example.onboardingself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnboardingselfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onboardingself/OnboardingselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onboardingself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnboardingselfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
